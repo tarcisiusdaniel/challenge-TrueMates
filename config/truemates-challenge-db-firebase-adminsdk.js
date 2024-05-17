e34f3c78939c52be7e81f77f040c0ded2a40c11c
Tarcisius Daniel <tarcisiusdaniel@gmail.com>
2024-05-16 17:13:22 -0700

    firebase creds added

---
+++ config/truemates-challenge-db-firebase-adminsdk.js
@@ -0,0 +1,13 @@
+export default{
+  "type": "service_account",
+  "project_id": "truemates-challenge-db",
+  "private_key_id": "14e9140bdae225321e7b0767076d4dc6af69d3ff",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDX9S8pNhBRrJVX\n87zZYaNwYa0OGtuXMf5xt/2VWQbYReht3y5rywEXvyJJbQxPsrosbz8N4999TrJ1\noYzlKtxPToLW0DoJOdAmUqLDxmOsFBt7SkfyXZjVxUJ+/H6NfgOAvQj3Ze4nKlCi\nD0igyixDebgd0canfZXdW/5IUvUJfy7fiS/aNT3U8sQMdFB6fYJ8VeLhTHcoVrAq\n9whn/miJ+sZPkWJLSRdQVh4T+YjU1nZs9I604+VtSXO3WOL2Fcnm7CwnViH4GGIr\nbe2qaKgbLiQrCVfcArzorA6B59QyCCsCeeXjhXNtoNtKiVX3q4y3VyJc3TfSJNnY\niyHsEHrZAgMBAAECggEACfJShkDltoVO6Zqhz5dWKkZtbJFjHM8iK4WwadEMZiiu\nIGVfo3pZpgjZIO2jXAZc2dAT5OVidUiLIqzaqvFy7Tot/4veqG8fvawfsAmyrInO\njDqrHqgd8naf7e5eO9EagzLxt0dcFTS3OweeLj2xlTgFeWYgkrVPkj768sTTYWkD\nBvQCxQA/uotUF96uSkRDJ8L8GG767+JVUdR8GSi76jtRlrlKxdxvRTVhs2PEyj51\nKDED4tsSQaoiOVyoRCLulfsnfz2dm2qlnPF3v3ji4aF8yom02yBPEKlTtD2M6XsJ\n+dqN3IfIF6iakw1VbBM+/qxMfb1WaEHhmm5LiYBKCQKBgQD1wgTP3NWQout22SPp\n0UPGRGm4pKqJt0thhE1hO17DLpgHoeSY6nVd4XWDb0S5yDDKIX5exocM8v9FRDUv\njF600wKKA2x7XIkikCtpCZzNfUWiOtA+OeqWHOI/WJSio1NV7QxhcHg91GeNihX8\n6uw7c/9zJ2NdL8B/S9TKuDOfTQKBgQDg9TqdDVviDL/hLXs3d/I/Kd/hLf8Pa2x7\ni1P1UJjwi1N+hAsNuO1pJeKa1RfaHbcnAK/PLGexGw6ck4wMkOxTCgMJMZmaTrMp\nwprfzeduBAfvuSOROt2+T+6V/ljSDvGaRt6i2MT7or+UxGbs4cbRNNCnBXzlDtcH\nc5MayuHbvQKBgEJlWrt15FUhTxBa9fLz0KW+UfnKszBlzv3Xqrg8PAvriLN3L7WI\nkpD8mq0Z4K14woW5qtScOlxrQkBKfzwLZoLgB22WIzbo9zvZS4EjUbPKxodf8Gb/\naruHrjCDFBUknx014ARrSHEb86YvymT8azpcyoUn/RZuEknSCPmD7p55AoGBAIdX\nYrU8ZUxhyUtrdpdPpcIUqmoqpyqawcCueYu8utb1q/0Q6imOWd+J+ISYsen7OMbR\nZfSpzZTDJ45SQs8InfXzrxHn8nbg5G+6cj01/we0iyXR6DEnDmPCaVN6m+t9LCRa\ngPXOnuyZ5NNXOmlICLqzINlrttGElSAZi6Zx62KtAoGAOZza7PHiYDglG+TDBW8E\nKokq4tsOLkpVIaR2YiUrPGS4j7Jt3gdK4gncifHo2CEidzInX0lTzjSJN6kwPMsu\nxEPrZFFlVdjV1z9fcqSCR+oSTyzoELpEn53RXNnLTz2LknzXvJ0tVMZMzQyWS/ZS\nvcZyBpifvJ1i4OpKpH0uui4=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-nn65f@truemates-challenge-db.iam.gserviceaccount.com",
+  "client_id": "110317315923128439745",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nn65f%40truemates-challenge-db.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
